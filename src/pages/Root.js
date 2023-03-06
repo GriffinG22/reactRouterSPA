da0b67fbf113463d38e29aed7b4e590b3c9923a4
Griffin Gradke <griffin.gradke@gmail.com>
2023-03-06 14:16:43 -0600

    added RootLayout

---
+++ src/pages/Root.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+  return (
+    <>
+        <h1>Root Layout</h1>
+        <Outlet />
+    </>
+  )
+}
+
+export default RootLayout;
